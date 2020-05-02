ec51e75401fdbe883dc1aa6d50eb5075feea48a9
Timothée Haudebourg <timothee.haudebourg@irisa.fr>
2020-05-02 15:13:03 +0200

    New optimized containers.

--- src/syntax/container.rs
+++ src/syntax/container.rs
@@ -1,6 +1,6 @@
 use std::convert::TryFrom;
 
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
 pub enum ContainerType {
 	Graph,
 	Id,
@@ -29,72 +29,156 @@ impl<'a> TryFrom<&'a str> for ContainerType {
 	}
 }
 
-#[derive(Clone, PartialEq, Eq, Hash)]
-pub struct Container(Vec<ContainerType>);
+impl From<ContainerType> for Container {
+	fn from(c: ContainerType) -> Container {
+		use ContainerType::*;
+		match c {
+			Graph => Container::Graph,
+			Id => Container::Id,
+			Index => Container::Index,
+			Language => Container::Language,
+			List => Container::List,
+			Set => Container::Set,
+			Type => Container::Type
+		}
+	}
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Container {
+	// Empty container
+	None,
+
+	Graph,
+	Id,
+	Index,
+	Language,
+	List,
+	Set,
+	Type,
+
+	GraphSet,
+	GraphId,
+	GraphIndex,
+	IdSet,
+	IndexSet,
+	LanguageSet,
+	TypeSet,
+
+	GraphIdSet,
+	GraphIndexSet
+}
 
 impl Container {
 	pub fn new() -> Container {
-		Container(Vec::new())
+		Container::None
+	}
+
+	pub fn from<'a, I: IntoIterator<Item = &'a ContainerType>>(iter: I) -> Result<Container, ContainerType> {
+		let mut container = Container::new();
+		for item in iter {
+			if !container.add(*item) {
+				return Err(*item)
+			}
+		}
+
+		Ok(container)
+	}
+
+	pub fn as_slice(&self) -> &[ContainerType] {
+		use Container::*;
+		match self {
+			None => &[],
+			Graph => &[ContainerType::Graph],
+			Id => &[ContainerType::Id],
+			Index => &[ContainerType::Index],
+			Language => &[ContainerType::Language],
+			List => &[ContainerType::List],
+			Set => &[ContainerType::Set],
+			Type => &[ContainerType::Type],
+			GraphSet => &[ContainerType::Graph, ContainerType::Set],
+			GraphId => &[ContainerType::Graph, ContainerType::Id],
+			GraphIndex => &[ContainerType::Graph, ContainerType::Index],
+			IdSet => &[ContainerType::Id, ContainerType::Set],
+			IndexSet => &[ContainerType::Index, ContainerType::Set],
+			LanguageSet => &[ContainerType::Language, ContainerType::Set],
+			TypeSet => &[ContainerType::Type, ContainerType::Set],
+			GraphIdSet => &[ContainerType::Graph, ContainerType::Id, ContainerType::Set],
+			GraphIndexSet => &[ContainerType::Graph, ContainerType::Index, ContainerType::Set]
+		}
+	}
+
+	pub fn iter(&self) -> impl Iterator<Item = &ContainerType> {
+		self.as_slice().iter()
 	}
 
 	pub fn len(&self) -> usize {
-		self.0.len()
+		self.as_slice().len()
 	}
 
 	pub fn is_empty(&self) -> bool {
-		self.0.is_empty()
+		match self {
+			Container::None => true,
+			_ => false
+		}
 	}
 
 	pub fn contains(&self, c: ContainerType) -> bool {
-		self.0.contains(&c)
+		self.as_slice().contains(&c)
+	}
+
+	pub fn with(&self, c: ContainerType) -> Option<Container> {
+		let new_container = match (self, c) {
+			(Container::None, c) => c.into(),
+			(Container::Graph, ContainerType::Graph) => *self,
+			(Container::Graph, ContainerType::Set) => Container::GraphSet,
+			(Container::Graph, ContainerType::Id) => Container::GraphId,
+			(Container::Graph, ContainerType::Index) => Container::GraphIndex,
+			(Container::Id, ContainerType::Id) => *self,
+			(Container::Id, ContainerType::Graph) => Container::GraphId,
+			(Container::Id, ContainerType::Set) => Container::IdSet,
+			(Container::Index, ContainerType::Index) => *self,
+			(Container::Index, ContainerType::Graph) => Container::GraphIndex,
+			(Container::Language, ContainerType::Language) => *self,
+			(Container::Language, ContainerType::Set) => Container::LanguageSet,
+			(Container::List, ContainerType::List) => *self,
+			(Container::Set, ContainerType::Set) => *self,
+			(Container::Set, ContainerType::Graph) => Container::GraphSet,
+			(Container::Set, ContainerType::Id) => Container::IdSet,
+			(Container::Set, ContainerType::Index) => Container::IndexSet,
+			(Container::Set, ContainerType::Language) => Container::LanguageSet,
+			(Container::Set, ContainerType::Type) => Container::TypeSet,
+			(Container::Type, ContainerType::Type) => *self,
+			(Container::Type, ContainerType::Set) => Container::TypeSet,
+			(Container::GraphSet, ContainerType::Graph) => *self,
+			(Container::GraphSet, ContainerType::Set) => *self,
+			(Container::GraphSet, ContainerType::Id) => Container::GraphIdSet,
+			(Container::GraphSet, ContainerType::Index) => Container::GraphIdSet,
+			(Container::GraphId, ContainerType::Graph) => *self,
+			(Container::GraphId, ContainerType::Id) => *self,
+			(Container::GraphId, ContainerType::Set) => Container::GraphIdSet,
+			(Container::GraphIndex, ContainerType::Graph) => *self,
+			(Container::GraphIndex, ContainerType::Index) => *self,
+			(Container::GraphIndex, ContainerType::Set) => Container::GraphIndexSet,
+			(Container::GraphIdSet, ContainerType::Graph) => *self,
+			(Container::GraphIdSet, ContainerType::Id) => *self,
+			(Container::GraphIdSet, ContainerType::Set) => *self,
+			(Container::GraphIndexSet, ContainerType::Graph) => *self,
+			(Container::GraphIndexSet, ContainerType::Index) => *self,
+			(Container::GraphIndexSet, ContainerType::Set) => *self,
+			_ => return None
+		};
+
+		Some(new_container)
 	}
 
 	pub fn add(&mut self, c: ContainerType) -> bool {
-		if self.is_empty() {
-			self.0.push(c);
-			true
-		} else if self.contains(c) {
-			true
-		} else {
-			use ContainerType::*;
-			let valid = if self.len() == 1 {
-				match (self.0.first().unwrap(), c) {
-					(Set, Index) => true,
-					(Set, Graph) => true,
-					(Set, Id) => true,
-					(Set, Type) => true,
-					(Set, Language) => true,
-					(Index, Set) => true,
-					(Graph, Set) => true,
-					(Id, Set) => true,
-					(Type, Set) => true,
-					(Language, Set) => true,
-					//
-					(Graph, Id) => true,
-					(Id, Graph) => true,
-					(Graph, Index) => true,
-					(Index, Graph) => true,
-					//
-					_ => false
-				}
-			} else if self.len() == 2 {
-				match c {
-					Set if self.contains(Graph) && (self.contains(Id) || self.contains(Index)) => true,
-					Graph if self.contains(Set) && (self.contains(Id) || self.contains(Index)) => true,
-					Id if self.contains(Graph) && self.contains(Set) => true,
-					Index if self.contains(Graph) && self.contains(Set) => true,
-					_ => false
-				}
-			} else {
-				false
-			};
-
-			if valid {
-				self.0.push(c);
+		match self.with(c) {
+			Some(container) => {
+				*self = container;
 				true
-			} else {
-				false
-			}
+			},
+			None => false
 		}
 	}
 }
