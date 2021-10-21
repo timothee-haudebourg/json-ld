8a3f5a6f76c9b8a76c18a448d75164f7a9c56c0d
Timothée Haudebourg <timothee.haudebourg@hemisemi.fr>
2021-10-21 22:31:24 +0200

    Run fmt.

--- src/expansion/node.rs
+++ src/expansion/node.rs
@@ -96,7 +96,7 @@ where
 }
 
 /// Type returned by the `expand_node_entries` function.
-/// 
+///
 /// It is a tuple containing both the node being expanded
 /// and a boolean flag set to `true` if the node contains
 /// value object entries (in practice, if it has a `@language` entry).
