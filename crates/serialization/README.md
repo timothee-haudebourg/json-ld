4b1e0d730a9af04d5d055cc3cdd22c9407c7f006
Timothée Haudebourg <timothee.haudebourg@spruceid.com>
2024-04-09 15:18:36 +0200

    Document `json-ld-serialization`.

---
+++ crates/serialization/README.md
@@ -0,0 +1,11 @@
+# JSON-LD serialization.
+
+<!-- cargo-rdme start -->
+
+This crate implements JSON-LD serialization (from RDF dataset to JSON-LD)
+through the [`linked_data`](https://github.com/spruceid/linked-data-rs)
+crate.
+The input value can be an RDF dataset, or any type implementing 
+[`linked_data::LinkedData`].
+
+<!-- cargo-rdme end -->
--- crates/serialization/src/lib.rs
+++ crates/serialization/src/lib.rs
@@ -1,3 +1,8 @@
+//! This crate implements JSON-LD serialization (from RDF dataset to JSON-LD)
+//! through the [`linked_data`](https://github.com/spruceid/linked-data-rs)
+//! crate.
+//! The input value can be an RDF dataset, or any type implementing 
+//! [`linked_data::LinkedData`].
 use std::hash::Hash;
 
 use json_ld_core::{ExpandedDocument, Node, Object};
